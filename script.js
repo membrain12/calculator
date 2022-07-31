b5c62fcc4afd386570eeca6a9bf569023927c50a
Paul Campbell <apcampbell12@gmail.com>
2022-07-31 14:22:15 -0500

    Add operate function for use with others

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(op, first, sec) {
+    if (typeof op != 'string') {
+        return;
+    } else if (op == '+') {
+        add(first, sec)
+    } else if (op == '-') {
+        subtract(first, sec);
+    } else if (op == '*') {
+        multiply(first, sec);
+    } else if (op == '/') {
+        divide(first, sec);
+    } else {
+        console.log('not recognized');
+    }
+}
